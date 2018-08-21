aec141c2e4c5de591e5eab20be934e4b53e0b24f
Samir Hajiyev <samir.hajiyev@snow.dog>
2018-08-21 22:12:16 +0400

    #5 extend BlockInterface from core BlockInterface

--- Api/Data/BlockInterface.php
+++ Api/Data/BlockInterface.php
@@ -6,62 +6,13 @@ namespace Snowdog\CmsApi\Api\Data;
  * CMS block interface.
  * @api
  */
-interface BlockInterface
+interface BlockInterface extends Magento\Cms\Api\Data\BlockInterface
 {
     /**#@+
      * Constants for keys of data array. Identical to the name of the getter in snake case
      */
-    const BLOCK_ID      = 'block_id';
-    const IDENTIFIER    = 'identifier';
-    const TITLE         = 'title';
-    const CONTENT       = 'content';
-    const CREATION_TIME = 'creation_time';
-    const UPDATE_TIME   = 'update_time';
-    const IS_ACTIVE     = 'is_active';
-    const STORES        = 'stores';
+    const STORES = 'stores';
     /**#@-*/
-
-    /**
-     * Get ID
-     *
-     * @return int|null
-     */
-    public function getId();
-
-    /**
-     * Get identifier
-     *
-     * @return string
-     */
-    public function getIdentifier();
-
-    /**
-     * Get title
-     *
-     * @return string|null
-     */
-    public function getTitle();
-
-    /**
-     * Get content
-     *
-     * @return string|null
-     */
-    public function getContent();
-
-    /**
-     * Get creation time
-     *
-     * @return string|null
-     */
-    public function getCreationTime();
-
-    /**
-     * Get update time
-     *
-     * @return string|null
-     */
-    public function getUpdateTime();
 
     /**
      * Get store ids
@@ -71,71 +22,8 @@ interface BlockInterface
     public function getStores();
 
     /**
-     * Is active
-     *
-     * @return bool|null
-     */
-    public function isActive();
-
-    /**
-     * Set ID
-     *
-     * @param int $id
-     * @return BlockInterface
-     */
-    public function setId($id);
-
-    /**
-     * Set identifier
-     *
-     * @param string $identifier
-     * @return BlockInterface
-     */
-    public function setIdentifier($identifier);
-
-    /**
-     * Set title
-     *
-     * @param string $title
-     * @return BlockInterface
-     */
-    public function setTitle($title);
-
-    /**
-     * Set content
-     *
-     * @param string $content
-     * @return BlockInterface
-     */
-    public function setContent($content);
-
-    /**
-     * Set creation time
-     *
-     * @param string $creationTime
-     * @return BlockInterface
-     */
-    public function setCreationTime($creationTime);
-
-    /**
-     * Set update time
-     *
-     * @param string $updateTime
-     * @return BlockInterface
-     */
-    public function setUpdateTime($updateTime);
-
-    /**
      * @param array $storeIds
      * @return BlockInterface
      */
     public function setStores($storeIds);
-
-    /**
-     * Set is active
-     *
-     * @param bool|int $isActive
-     * @return BlockInterface
-     */
-    public function setIsActive($isActive);
 }
